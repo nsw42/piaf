73b9eeea85daf5d6f3dfeacdc50b3abcac5c4716
nsw42 <neil@wynded.co.uk>
2025-07-09 21:57:22 +0100

    Add a simple footer to pause/resume, based on polling the server

--- src/assets/piaf.js
+++ src/assets/piaf.js
@@ -1,3 +1,6 @@
+let resumeButton;
+let pauseButton;
+
 function initPiaf() {
     document.querySelectorAll(".piaf-play-file").forEach(btn => {
         btn.addEventListener("click", () => {
@@ -6,4 +9,38 @@ function initPiaf() {
             })
         })
     })
+
+    document.querySelector('#pause').addEventListener("click", () => {
+        fetch("/player/pause", { method: "PUT" })
+    })
+
+    document.querySelector('#resume').addEventListener("click", () => {
+        fetch("/player/resume", { method: "PUT" })
+    })
+
+    pauseButton = document.getElementById('pause')
+    resumeButton = document.getElementById('resume')
+
+    setTimeout(updateNowPlaying, 1000)
+}
+
+async function updateNowPlaying() {
+    const response = await fetch("/player/status")
+    const data = await response.json()
+    switch (data['state']) {
+        case 'stopped':
+            pauseButton.setAttribute('disabled', 'disabled')
+            resumeButton.setAttribute('disabled', 'disabled')
+            break
+        case 'paused':
+            pauseButton.setAttribute('disabled', 'disabled')
+            resumeButton.removeAttribute('disabled')
+            break
+        case 'playing':
+            pauseButton.removeAttribute('disabled')
+            resumeButton.setAttribute('disabled', 'disabled')
+            break
+    }
+
+    setTimeout(updateNowPlaying, 1000)
 }

70c7be540216454858b50adaf8a1e7e23da92260
nsw42 <neil@wynded.co.uk>
2025-07-10 23:14:16 +0100

    Add scripts to do the cross-compile to aarch64 Alpine Linux (Raspberry Pi 4)

---
+++ build_for_pi.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+PLATFORM=aarch64
+cd $(dirname $0)
+IMAGE_NAME=piaf-$PLATFORM
+docker build --platform linux/$PLATFORM -f Dockerfile.pi -t $IMAGE_NAME .
+
+container=$(docker create $IMAGE_NAME)
+rm -rf dist/$PLATFORM
+mkdir -p dist/$PLATFORM
+docker cp $container:/go/src/dist/piaf ./dist/$PLATFORM/piaf
+docker rm -v $container > /dev/null

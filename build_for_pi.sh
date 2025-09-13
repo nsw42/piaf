8a38e0d29e449aed726069224646f17f7eea450c
nsw42 <neil@wynded.co.uk>
2025-09-13 11:05:52 +0100

    Add support for other cross-build variants

--- build_for_pi.sh
+++ build_for_pi.sh
@@ -1,13 +1,37 @@
 #! /bin/bash
 
+usage() {
+  echo "$0 [PLATFORM] [CROSS_BUILDER_IMAGE]"
+  exit 0
+}
+
 set -e
 
 npm run make_css
 
-PLATFORM=aarch64
+if [ -z "$1" ]; then
+  PLATFORM=aarch64
+elif [ "$1" = "-h" -o "$1" = "--help" ]; then
+  usage
+else
+  PLATFORM=$1
+fi
+
+if [ -z "$2" ]; then
+  CROSS_BUILDER=go-cross-builder-1.24-alpine3.22-$PLATFORM
+else
+  CROSS_BUILDER=$2
+fi
+
+if [ -z "$3" ]; then
+  TARGETPLATFORM=linux/$PLATFORM
+else
+  TARGETPLATFORM=$3
+fi
+
 cd $(dirname $0)
 IMAGE_NAME=piaf-$PLATFORM
-docker build --platform linux/$PLATFORM -f Dockerfile.pi -t $IMAGE_NAME .
+docker build --platform $TARGETPLATFORM --build-arg CROSS_BUILDER=$CROSS_BUILDER -f Dockerfile.pi -t $IMAGE_NAME .
 
 container=$(docker create $IMAGE_NAME)
 rm -rf dist/$PLATFORM

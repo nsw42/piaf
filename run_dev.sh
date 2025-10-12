19cd54088b73916679df8d7f0ff5969e3c8bf67f
nsw42 <neil@wynded.co.uk>
2025-10-12 11:30:40 +0100

    Correct arg handling in wrapper script

--- run_dev.sh
+++ run_dev.sh
@@ -3,7 +3,7 @@
 set -e
 
 ARGS=("$@")
-N=$(( ${#ARGS} - 1))
+N=$(( ${#ARGS[@]} - 1))
 for i in `seq 0 $N`; do
   if [ "${ARGS[$i]}" == "-d" ]; then
     j=$(( i+1 ))
@@ -15,4 +15,3 @@ cd "`dirname "$0"`/src"
 export CGO_CXXFLAGS="`pkg-config --cflags soundtouch`"
 export CGO_LDFLAGS="`pkg-config --libs soundtouch`"
 go run -ldflags="-extldflags=-Wl,-no_warn_duplicate_libraries" . "${ARGS[@]}"
-
